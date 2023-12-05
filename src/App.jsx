eea653daeb9c46e3b86559838a59e09b3fa2c55a
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-12-05 18:18:13 +0200

    Adding tutorial page and footer; figuring out the icon in title

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,9 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                {/* <Route
-                    path="/contact"
-                    element={<Contact />}
-                />
-                <Route path="/blogs" element={<Blogs />} />
-                <Route
-                    path="/sign-up"
-                    element={<SignUp />}
-                /> */}
+                <Route path="/tuts" element={<Tutorials />} />
             </Routes>
+            <Footer />
         </Router>
     );
 }
