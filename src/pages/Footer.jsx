395fe83a02d2548ed6b1cb8a2eb7d0f1201d0ede
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-12-30 11:10:38 +0200

    Add icons and set base structure for footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,10 +4,22 @@ function Footer(){
     return(
         <div className="container">
            <div className="row">
-            <div className="col-6">Hello, i'm footer</div>
-            <div className="col-2">1</div>
-            <div className="col-2">2</div>
-            <div className="col-2">3</div>
+            <div className="col-6">Made by yours truly, f.</div>
+            <div className="col-2">
+                <a href="https://portfolio-ppk.netlify.app/" title="Portfolio" target="_blank">
+                    <i className="fa-solid fa-terminal" style={{color: "#090a0b;"}}></i>
+                </a>
+            </div>
+            <div className="col-2">
+                <a href="mailto:ilaphi@outlook.com" title="Mail" target="_blank">
+                <i class="fa-regular fa-envelope" style={{color: "#090a0b;"}}></i>
+                </a>
+            </div>
+            <div className="col-2">
+                <a href="https://github.com/PKuzi01" title="Github" target="_blank">
+                    <i className="fa-brands fa-github" style={{color: "#090a0b;"}}></i>
+                </a>
+            </div>
            </div>
         </div>
     )
