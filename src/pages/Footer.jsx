f6aaab6b4075f2b04fc3fbd7535090b086857b41
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-12-30 13:45:22 +0200

    fix icon sizes and position

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import "../assets/style/Footer.css";
 
 function Footer(){
     return(
         <div className="container">
            <div className="row">
-            <div className="col-6">Made by yours truly, f.</div>
-            <div className="col-2">
+            <div className="col-6">
+                <p className="footer-text">Made by yours truly, f.</p>
+            </div>
+            <div className="col-2 footer-icon">
                 <a href="https://portfolio-ppk.netlify.app/" title="Portfolio" target="_blank">
-                    <i className="fa-solid fa-terminal" style={{color: "#090a0b;"}}></i>
+                    <i className="fa-solid fa-terminal" style={{color: "#090a0b"}}></i>
                 </a>
             </div>
-            <div className="col-2">
+            <div className="col-2 footer-icon">
                 <a href="mailto:ilaphi@outlook.com" title="Mail" target="_blank">
                 <i class="fa-regular fa-envelope" style={{color: "#090a0b;"}}></i>
                 </a>
             </div>
-            <div className="col-2">
+            <div className="col-2 footer-icon">
                 <a href="https://github.com/PKuzi01" title="Github" target="_blank">
                     <i className="fa-brands fa-github" style={{color: "#090a0b;"}}></i>
                 </a>
